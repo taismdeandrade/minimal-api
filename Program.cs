05c89211fff3e0625b334d7673d9375652a597e1
TaisAndrade <taismdeandrade@gmail.com>
2025-09-02 17:32:00 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using MinimalAPI.Dominio.DTO;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "1234")
+    {
+        return Results.Ok("Login bem sucedido.");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+
+app.Run();
